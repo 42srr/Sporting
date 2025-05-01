ae7454ed501200b9c83d8680a8e05772df800172
ElroyKR <elroykr@gmail.com>
2025-05-01 17:50:33 +0900

    chore: Swagger 세팅

---
+++ src/main/java/kr/sporting/config/SwaggerConfig.java
@@ -0,0 +1,18 @@
+package kr.sporting.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("스포팅 API Spec")
+                        .version("v1")
+                );
+    }
+}
