db9b9e367b3057ff2a921c3ad91367aba408529b
ElroyKR <elroykr@gmail.com>
2025-05-04 17:47:04 +0900

    chore: remove unnecessary annotation

--- src/main/java/kr/sporting/user/dto/request/CreateUserRequest.java
+++ src/main/java/kr/sporting/user/dto/request/CreateUserRequest.java
@@ -5,14 +5,12 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PastOrPresent;
 import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
 @Schema(description = "회원가입 요청")
 public record CreateUserRequest(
         @NotNull
-        @Size(min = 3, max = 12)
         @Pattern(regexp = "^(?=.*[a-z0-9가-힣])[a-z0-9가-힣]{3,12}$")
         @Schema(description = "닉네임")
         String nickname,
