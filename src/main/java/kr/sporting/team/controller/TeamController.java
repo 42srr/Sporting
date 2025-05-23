81d7707dd72af2f52a953463f26520505bb87058
yeonjunky <yeonjunky@gmail.com>
2025-05-23 16:02:35 +0900

    feat: Controller respond with data contained body and add swagger description

--- src/main/java/kr/sporting/team/controller/TeamController.java
+++ src/main/java/kr/sporting/team/controller/TeamController.java
@@ -1,5 +1,7 @@
 package kr.sporting.team.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import kr.sporting.team.domain.Team;
 import kr.sporting.team.dto.CreateTeamRequest;
@@ -12,16 +14,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "Team API")
+@RequestMapping("/teams")
 public class TeamController {
     private final TeamService teamService;
 
-    @GetMapping("/teams")
-    public ResponseEntity<Team> getTeams() {
-        teamService.getTeams();
-        return ResponseEntity.ok().build();
+    @Operation(summary = "전체 팀 조회")
+    @GetMapping
+    public ResponseEntity<Iterable<Team>> getTeams() {
+        Iterable<Team> teams = teamService.getTeams();
+        return ResponseEntity.ok(teams);
     }
 
-    @GetMapping("/teams/{id}")
+    @Operation(summary = "팀 조회")
+    @GetMapping("/{id}")
     public ResponseEntity<Team> getTeam(@PathVariable Long id) {
         try {
             Team team = teamService.getTeamById(id);
@@ -31,23 +37,25 @@ public class TeamController {
         }
     }
 
-    @PostMapping("/teams")
+    @Operation(summary = "팀 생성")
+    @PostMapping
     public ResponseEntity<Team> createTeam(@RequestBody @Valid CreateTeamRequest request) {
-        teamService.createTeam(request.toEntity());
-        return ResponseEntity.ok().build();
+        Team team = teamService.createTeam(request.toEntity());
+        return ResponseEntity.ok(team);
     }
 
-    @PutMapping("/teams/{id}")
+    @Operation(summary = "팀 정보 수정")
+    @PutMapping("/{id}")
     public ResponseEntity<Team> updateTeam(@PathVariable Long id, @RequestBody @Valid UpdateTeamRequest request) {
-        teamService.updateTeam(id, request);
-        return ResponseEntity.ok().build();
+        Team team = teamService.updateTeam(id, request);
+        return ResponseEntity.ok(team);
     }
 
-    @DeleteMapping("/teams/{id}")
+    @Operation(summary = "팀 삭제")
+    @DeleteMapping("/{id}")
     public ResponseEntity<Team> deleteTeam(@PathVariable Long id) {
         teamService.deleteTeam(id);
         return ResponseEntity.ok().build();
     }
-    
 }
 
